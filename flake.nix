337095c2f0d19fb2bbfd423db63bd8b1c0702c00
orhnk <utfeightt@gmail.com>
2024-08-12 15:14:55 +0300

    build: update flakes

--- flake.nix
+++ flake.nix
@@ -1,11 +1,16 @@
 {
   description = "Flutter 3.13.x";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    android-nixpkgs = {
+      url = "github:tadfisher/android-nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = {
     self,
+    android-nixpkgs,
     nixpkgs,
     flake-utils,
   }:
@@ -17,20 +22,33 @@
           allowUnfree = true;
         };
       };
+
+      android-pkgs = import android-nixpkgs {
+        inherit system;
+      };
+
+      jdk-custom = pkgs.jdk17;
+      android-sdk-custom = android-pkgs.sdk (sdkPkgs:
+        with sdkPkgs; [
+          cmdline-tools-latest
+          build-tools-34-0-0
+          platform-tools
+          platforms-android-34
+          emulator
+        ]);
     in {
       devShell = with pkgs;
         mkShell rec {
-          ANDROID_SDK_ROOT = "${androidenv.androidPkgs_9_0.androidsdk}/libexec/android-sdk";
+          JAVA_HOME = jdk-custom;
+          ANDROID_HOME = "${android-sdk-custom}/share/android-sdk";
+          ANDROID_SDK_HOME = "${android-sdk-custom}/share/android-sdk";
+          ANDROID_SDK_ROOT = "${android-sdk-custom}/libexec/android-sdk";
+          CHROME_EXECUTABLE = "${pkgs.chromium}/bin/chromium";
           buildInputs = [
             flutter
-            androidenv.androidPkgs_9_0.androidsdk
-            jdk17
+            android-sdk-custom
+            jdk-custom
           ];
-          shellHook = ''
-            # Set the default shell
-            export SHELL=$(getent passwd $USER | cut -d: -f7)
-            exec "$SHELL"
-          '';
         };
     });
 }
